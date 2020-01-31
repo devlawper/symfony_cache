d89924a038032ec3680058ea8bbb1b9397dc377d
Laurent Pichon <tech@lescachotieres.com>
2020-01-31 07:04:36 +0100

    Exemple de mise en cache

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# cours-symfony-cache
-Code pour la vidéo sur le Cache dans Symfony
+# symfony-cache
+Le Cache dans Symfony
 
 # Installation :
 1. Clonez le dépôt
 2. Lancez le serveur
-3. Profitez de la vidéo :)
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,38 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param Stopwatch $stopwatch
+     * @param CacheInterface $cache
+     * @return Response
+     *
+     * StopWatch permet de timer et d'afficher les actions dans le profiler
+     * On crée une instance de CacheInterface pour gérer le cache
      */
-    public function index(Stopwatch $stopwatch)
+    public function index(Stopwatch $stopwatch, CacheInterface $cache)
     {
         $stopwatch->start('calcul-long');
 
         // On imagine un calcul ou un traitement long
-        $resultatCalcul = $this->fonctionQuiPrendDuTemps();
+        // $resultatCalcul = $this->fonctionQuiPrendDuTemps();
+
+        // Avec get() si la clé déclarée n'existe pas dans le cache, on la crée
+        $resultatCaclul = $cache->get('resulat-calcul-long', function (ItemInterface $item) {
+            // $item représente la boite qui contient ce cache, permet par exemple de définir un temps d'expiration
+            $item->expiresAfter(10);
+
+            return $this->fonctionQuiPrendDuTemps();
+        });
 
         $stopwatch->stop('calcul-long');
 
